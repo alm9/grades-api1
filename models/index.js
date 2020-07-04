ba0959071713714c866490d6f7e7870049faebb0
andre.machado <4ndre.machado@gmail.com>
2020-07-04 15:25:24 -0300

    data model created with mongoose

--- models/index.js
+++ models/index.js
@@ -1,6 +1,16 @@
 import mongoose from 'mongoose';
 
-const db = {};
+const db = {
+  name: { type: String, required: true },
+  subject: { type: Number, required: true },
+  type: { type: Number, required: true },
+  value: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  lastModified: { type: Date, default: Date.now },
+};
 db.mongoose = mongoose;
 db.url = process.env.MONGODB;
 
